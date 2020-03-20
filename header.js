8db831de349737bdcb3378ebd4fd99f40897f3fd
coreder0704 <a04272017@gmail.com>
2020-03-21 02:33:55 +0900

    v1.38.2 header JQuery

---
+++ header.js
@@ -0,0 +1,33 @@
+$(function(){
+    var headerHigher = $('.header-higher'); 
+    var headerLower = $('.header-lower');
+
+    //表示位置を取得し変数に格納
+    var showPoint = $('#showPoint').offset();
+
+    //hide位置を取得し変数に格納
+    var hidePoint = $('#hidePoint').offset();
+
+    console.log('showPoint : '+ showPoint.top + 'px, ' + showPoint.left + 'px');
+    console.log('hidePoint : '+ hidePoint.top + 'px, ' + hidePoint.left + 'px');
+
+    $(window).scroll(function(){
+        //スクロールが表示位置の値より大きい場合ヘッダー表示
+        //スクロールがhide位置より大きい場合ヘッダーhide
+        if($(this).scrollTop() > showPoint.top){
+            headerHigher.fadeIn();
+            headerLower.fadeIn();
+        } else {
+            headerHigher.fadeOut();
+            headerLower.fadeOut();
+        }
+        /* if($(this).scrollTop() > showPoint && $(this).scrollTop() < hidePoint){
+            headerHigher.fadeIn();
+            headLower.fadeIn();
+        }else {
+            headerHigher.fadeOut();
+            headerLower.fadeOut();
+        } */
+    });
+
+});
