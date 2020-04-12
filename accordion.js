9064a922c8bcd81ca38700344a76dcd9efffe65b
coreder0704 <a04272017@gmail.com>
2020-04-13 00:09:56 +0900

    open and hide

--- accordion.js
+++ accordion.js
@@ -1,14 +1,15 @@
 $(function(){
-    // アコーディオンメニューを展開・収納する
-    // メニューボタンをクリックする
-    // if(acordion != '.show')
-    // accordionに'.show'を追加
-    // '.show' is slided down and showed
-    // アコーディオンが上から下に展開される
 
-    // else
-    // accordionの'.show'を削除
-    // '.accordion' is slided up and hided
-    // アコーディオンが下から上へ収納される
+    // アコーディオンメニューを展開・収納する
+    $('.accordion-button').click(function(){
+        if($('.accordion').hasClass('.open')){
+            $('.accordion').removeClass('.open').slideUp().fadeOut();
+        }else {
+            $('.accordion').addClass('.open').slideDown().show();
+        }
+
+        // accordion-buttonを一時的に最上位レイヤに表示
+        // accordion-buttonの色を変える
+    });
     
 });
