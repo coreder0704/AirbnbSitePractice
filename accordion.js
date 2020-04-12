9cece12ada61de1ba4d5ae5a196d94fc4377e6e4
coreder0704 <a04272017@gmail.com>
2020-04-12 23:17:49 +0900

    create accordion.js

---
+++ accordion.js
@@ -0,0 +1,14 @@
+$(function(){
+    // アコーディオンメニューを展開・収納する
+    // メニューボタンをクリックする
+    // if(acordion != '.show')
+    // accordionに'.show'を追加
+    // '.show' is slided down and showed
+    // アコーディオンが上から下に展開される
+
+    // else
+    // accordionの'.show'を削除
+    // '.accordion' is slided up and hided
+    // アコーディオンが下から上へ収納される
+    
+});
