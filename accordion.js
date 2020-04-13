6222e47eb6e894cfef245ad2035fce06bbecf132
coreder0704 <a04272017@gmail.com>
2020-04-14 01:52:53 +0900

    change icon-button color

--- accordion.js
+++ accordion.js
@@ -2,14 +2,18 @@ $(function(){
 
     // アコーディオンメニューを展開・収納する
     $('.accordion-button').click(function(){
-        if($('.accordion').hasClass('.open')){
-            $('.accordion').removeClass('.open').slideUp().fadeOut();
-        }else {
-            $('.accordion').addClass('.open').slideDown().show();
-        }
+        if($('.accordion').hasClass('open')){
+            $('.accordion-button').removeClass('button__changeColor');
 
-        // accordion-buttonを一時的に最上位レイヤに表示
-        // accordion-buttonの色を変える
+            $('.accordion').removeClass('open').slideUp();
+        }else {
+            // accordion-buttonを一時的に最上位レイヤに表示
+            // accordion-buttonの色を変える
+            $('.accordion-button').addClass('button__changeColor');
+
+            $('.accordion').addClass('open').slideDown();
+        }
+        
     });
     
 });
