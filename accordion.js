f0855d9d1d4bacffd4e2ba441617ef52cf2d3be2
coreder0704 <a04272017@gmail.com>
2020-04-16 01:47:51 +0900

    rotate angle icon

--- accordion.js
+++ accordion.js
@@ -6,16 +6,34 @@ $(function(){
             $('.accordion-button').removeClass('button__changeColor');
 
             $('.accordion').removeClass('open').slideUp();
+
+            $('.rotate').animate(
+                { opacity: 1 },{
+                    duration: 1000,
+                    step: function (now) {
+                        $(this).css({ transform: 'rotate(' + now * 0 + 'deg)' })
+                    }
+                }
+            );
         }else {
             // accordion-buttonを一時的に最上位レイヤに表示
             // accordion-buttonの色を変える
             $('.accordion-button').addClass('button__changeColor');
 
-            $('.accordion').addClass('open').slideDown();
+            $('.accordion').addClass('open').slideDown('slow');
+
+            // 下向き記号を180度回転させる
+            $('.rotate').animate({ opacity: 1 },{
+                duration: 1000,
+                step: function (now) {
+                    $(this).css({ transform: 'rotate(' + now * 180 + 'deg)' })
+                }
+            });
+
         }
         
         // 縦スクロールを禁止する
-        // 下向き記号を180度回転させる
+        
     });
     
 });
